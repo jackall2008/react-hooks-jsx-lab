13463fc7e1c621068c618164ae575fa61ad69978
Julian Patterson <julian.patterson@gmail.com>
2023-02-01 13:23:55 -0500

    The components folder contains 4 files I edited that pass 11 JSX tests

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h2>About Me</h2>
+    <p> I am a 35 year old attorney, entrepreneur, and philanthropist looking to make a difference in the global tech community!</p>
+    <img src={image} alt="I made this"></img>
+  </div>
+  );
 }
 
 export default About;
